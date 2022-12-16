4730319d59de13c97579a635a95f2a96cfe7f1de
Ignasius Yuda Adhitia <ignasius.yuda.adhitia@gmail.com>
2022-12-16 13:40:38 +0700

    fix: full width button loader component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,14 @@
 import React from "react";
+import Loader from "../components/Loader";
+import ProductCard from "../components/ProductCard";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <ProductCard />
+      <Loader />
+    </div>
+  );
 };
 
 export default Home;
