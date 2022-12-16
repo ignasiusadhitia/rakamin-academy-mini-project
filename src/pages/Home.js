3da23934901bb8e3d589dd6f421a63b748c60b0a
Ignasius Yuda Adhitia <ignasius.yuda.adhitia@gmail.com>
2022-12-16 20:25:57 +0700

    fix: ProductCard, Loader, and Home layout

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,12 +2,17 @@ import React from "react";
 import Loader from "../components/Loader";
 import ProductCard from "../components/ProductCard";
 
+const products = [1, 2, 3, 4, 5, 6, 7, 8];
+
 const Home = () => {
   return (
-    <div>
-      <ProductCard />
-      <Loader />
-    </div>
+    <>
+      <div className="container mx-auto px-4 grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-4">
+        {products.map((index) => (
+          <ProductCard key={index} />
+        ))}
+      </div>
+    </>
   );
 };
 
