222fe3f1ef37fb111cc6ed0bb479e178657ce98c
Ignasius Yuda Adhitia <ignasius.yuda.adhitia@gmail.com>
2022-12-17 11:35:21 +0700

    fix: active link style

--- src/components/MobileMenus.js
+++ src/components/MobileMenus.js
@@ -1,42 +1,50 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const MobileMenus = ({ hideMobileMenu }) => {
+  const menuItemSelected =
+    "block py-2 pl-3 pr-4 text-white bg-blue-700 rounded lg:bg-transparent lg:text-blue-700 lg:p-0 dark:text-white";
+  const menuItemNotSelected =
+    "block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 lg:hover:bg-transparent lg:border-0 lg:hover:text-blue-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent";
   return (
     <div
+      id="drawer-example"
       className="w-full bg-gray-900 bg-opacity-50 dark:bg-opacity-80 fixed inset-0 z-30"
+      tabIndex="-1"
+      aria-labelledby="drawer-label"
       onClick={hideMobileMenu}
     >
-      <ul
-        id="drawer-example"
-        className="h-screen p-4 md:text-sm md:font-medium  bg-white w-4/5 dark:bg-gray-800 "
-        tabIndex="-1"
-        aria-labelledby="drawer-label"
-      >
+      <ul className="h-screen p-4 bg-white w-4/5 dark:bg-gray-800 ">
         <li onClick={hideMobileMenu}>
-          <Link
+          <NavLink
             to="/"
-            className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white"
+            className={({ isActive }) =>
+              isActive ? menuItemSelected : menuItemNotSelected
+            }
             aria-current="page"
           >
             Home
-          </Link>
+          </NavLink>
         </li>
         <li onClick={hideMobileMenu}>
-          <Link
+          <NavLink
             to="/about"
-            className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+            className={({ isActive }) =>
+              isActive ? menuItemSelected : menuItemNotSelected
+            }
           >
             About
-          </Link>
+          </NavLink>
         </li>
         <li onClick={hideMobileMenu}>
-          <Link
+          <NavLink
             to="/contact"
-            className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+            className={({ isActive }) =>
+              isActive ? menuItemSelected : menuItemNotSelected
+            }
           >
             Contact
-          </Link>
+          </NavLink>
         </li>
       </ul>
     </div>
